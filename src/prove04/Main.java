3eb28f2ea687b3ea8a4379ac0655e71af4ae5481
git config --global user.name "Paul <moroniuntolehi@gmail.com>
2022-01-28 11:00:22 +0200

    git commit -m "Added my greetings to you."

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Patrick.");
+        System.out.println("Hello Patrick, I am Paul.");
+
     }
 }
